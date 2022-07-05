8e0bc07d07e4d72820d21b3e03bd994c0e02d91f
Alok <ashutoshdang2004@gmail.com>
2022-07-06 00:00:59 +0530

    Organise  code

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let questtion = document.getElementById("qs")
 let i
 let len = qs.length;
-for (i =0; i<len; i++){
-    qs[i].addEventListener('click',function(){
+for (i = 0; i < len; i++) {
+    qs[i].addEventListener('click', function () {
         this.classList.toggle('active');
         let answer = this.nextElementSibling;
-        if(answer.style.display == 'none'){
+        if (answer.style.display == 'none') {
             answer.style.display = 'block'
         }
-        else{
+        else {
             answer.style.display = 'none'
         }
-        
+
     })
 }
 
@@ -20,12 +20,12 @@ let bar = document.getElementById('bar')
 let menuBar = document.getElementById('menuBar')
 let barIcon = document.getElementById('barIcon')
 
-bar.addEventListener('click',()=>{
-    if(menuBar.style.right == "-600px"){
+bar.addEventListener('click', () => {
+    if (menuBar.style.right == "-600px") {
         menuBar.style.right = "60px"
     }
-    else{
+    else {
         menuBar.style.right = "-600px"
     }
-    
+
 })
